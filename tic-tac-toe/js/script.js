2fbd19872c24980c542423cd06fc3a4dd329aaa5
gayohignasius <gayohignasius>
2022-06-06 21:34:14 +0700

    add checking winner code

--- tic-tac-toe/js/script.js
+++ tic-tac-toe/js/script.js
@@ -1,19 +1,33 @@
 const box = document.querySelectorAll('.box');
-const restart = document.getElementById('restart');
+const restart = document.querySelector('.restart');
 const cross = 'cross';
 const circle = 'circle';
 const disabled = 'disabled';
+const gameOverElement = document.querySelector('.game-over');
+const gameOverText = document.querySelector('.game-over-text');
 let isCrossPlayerTurn = false;
 
+const winningCombination = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
 start();
-restart.addEventListener('click', restartGame);
+restart.addEventListener('click', start);
 
 function start() {
   isCrossPlayerTurn = true;
   box.forEach(box => {
     const removeCrossPlayer = box.classList.remove(cross, disabled);
     const removeCirclePlayer = box.classList.remove(circle, disabled);
-    box.addEventListener('click', handleClick, {once: true})
+    box.addEventListener('click', handleClick, { once: true });
+    gameOverElement.classList.remove('visible');
   })
 }
 
@@ -25,7 +39,13 @@ function handleClick(e) {
   const boxTarget = e.target;
   const currentClass = isCrossPlayerTurn ? cross : circle;
   placeMark(boxTarget, currentClass);
-  swapTurn();
+  if (checkWinning(currentClass)) {
+    endGame(false);
+  } else if (isDraw()) {
+    endGame(true);
+  } else {
+    swapTurn();
+  }
 }
 
 function placeMark(box, currentClass) {
@@ -36,3 +56,30 @@ function swapTurn() {
   isCrossPlayerTurn = !isCrossPlayerTurn;
 }
 
+function checkWinning(currentClass) {
+  return winningCombination.some(comb => {
+    return comb.every(id => {
+      return box[id].classList.contains(currentClass);
+    })
+  })
+}
+
+function endGame(draw) {
+  if (draw) {
+    gameOverText.innerText = "Draw!"
+  } else {
+    gameOverText.innerText = `Player ${isCrossPlayerTurn ? cross : circle} wins!`;
+  }
+  gameOverElement.classList.add('visible');
+  if (gameOverElement) {
+    box.forEach(box => {
+      box.removeEventListener('click', handleClick);
+    })
+  }
+}
+
+function isDraw() {
+	return [...box].every(box => {
+    return box.classList.contains(cross) || box.classList.contains(circle);
+	})
+}
