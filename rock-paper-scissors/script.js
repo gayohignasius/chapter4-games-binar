d546b34c74a76a1e90f1ee4f71fd5cf2f379937d
gayohignasius <gayohignasius>
2022-06-10 22:54:15 +0700

    add refresh button

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,5 +1,6 @@
 const playerOption = document.querySelectorAll('.playerOption');
 const computerOption = document.querySelectorAll('.computerOption');
+const arrayListOption = ['✊', '🖐', '✌'];
 
 class Player {
   constructor(name, opt) {
@@ -7,60 +8,149 @@ class Player {
     this.opt = opt;
   }
 
+
   get getName() {
-    return this._name;
+    return this.name;
   }
   set setName(newName) {
     newName = newName.trim();
     if (newName === '') {
         throw 'The name cannot be empty';
     }
-    this._name = newName;
+    this.name = newName;
   }
   get getOpt() {
     return this.opt;
   }
-  set setOpt(opt) {
-    opt = opt.value;
-    if (opt === '') {
+  set setOpt(option) {
+    if (option === '') {
         throw 'The option cannot be empty';
     }
-    this.opt = opt;
+    this.opt = option;
+  }
+
+  static getComputerOption() {
+    const computerOpt = arrayListOption[Math.floor(Math.random() * arrayListOption.length)];
+    return computerOpt;
+  }
+
+  static getResult(playerOpt, computerOpt, playerName, computerName) {
+    if (playerOpt == computerOpt) return this.result = 'DRAW';
+    if (playerOpt == '✊' ) return this.result = (computerOpt == '✌') ? `${playerName.toUpperCase()} <br> WIN` : `${computerName.toUpperCase()} <br> WIN`;
+    if (playerOpt == '🖐') return this.result = (computerOpt == '✊') ? `${playerName.toUpperCase()} <br> WIN` : `${computerName.toUpperCase()} <br> WIN`;
+    if (playerOpt == '✌') return this.result = (computerOpt == '🖐') ? `${playerName.toUpperCase()} <br> WIN` : `${computerName.toUpperCase()} <br> WIN`;
+  }
+
+}
+class Player1 extends Player {
+  constructor(name, opt) {
+    super(name, opt);
+  }
+
+  get getName() {
+    return this.name;
+  }
+  set setName(newName) {
+    newName = newName.trim();
+    if (newName === '') {
+        throw 'The name cannot be empty';
+    }
+    this.name = newName;
+  }
+  get getOpt() {
+    return this.opt;
+  }
+  set setOpt(option) {
+    if (option === '') {
+        throw 'The option cannot be empty';
+    }
+    this.opt = option;
+  }
+}
+class Computer extends Player {
+  constructor(name, opt) {
+    super(name, opt);
+  }
+
+  get getName() {
+    return this.name;
+  }
+  set setName(newName) {
+    newName = newName.trim();
+    if (newName === '') {
+        throw 'The name cannot be empty';
+    }
+    this.name = newName;
+  }
+  get getOpt() {
+    return this.opt;
+  }
+  set setOpt(option) {
+    if (option === '') {
+        throw 'The option cannot be empty';
+    }
+    this.opt = option;
   }
 }
 
 function startGame() {
   playerOption.forEach(opt => {
-    // opt.addEventListener('click', handleClick, { once: true });
     opt.addEventListener('click', handleClick);
   });
   
 }
+
 function handleClick(e) {
-  const player1 = new Player();
-  const computer = new Player();
+  const player1 = new Player1();
+  const computer = new Computer();
   
   const playerTargetOption = e.target;
-  player1.name = 'Player 1';
-  player1.opt = playerTargetOption.innerHTML;
+  player1.setName = 'Player 1';
+  player1.setOpt = playerTargetOption.innerHTML;
   console.log(player1);
   
-  const arrayListOption = [];
-  computerOption.forEach(opt => {
-    const computerListOption = opt.innerHTML;
-    arrayListOption.push(computerListOption);
-  });
+  let i = 0;
+  let computerTargetOption;
+  const computeTime = new Date().getTime();
 
-  const computerTargetOption = getComputerOption(arrayListOption);
-  computer.name = 'Computer'
-  computer.opt = computerTargetOption;
-  console.log(computer);
+  setInterval(() => {
+    if (new Date().getTime() - computeTime > 1000) {
+      clearInterval;
+      return;
+    }
+    computerTargetOption = Player.getComputerOption();
+    if (i == arrayListOption.length) i = 0;
+  }, 100);
+  
+  setTimeout(async () => {
+    computer.setName = 'Com';
+    computer.setOpt = computerTargetOption;
+    console.log(computer);
+    
+    for (let index = 0; index < computerOption.length; index++) {
+      computerOption.item(index).classList.remove('selected');
+      if (computerOption.item(index).innerHTML == computer.getOpt) {
+        const element = computerOption.item(index);
+        element.classList.add('selected');
+      }
+    }
 
-}
-
-function getComputerOption(list) {
-  const computerOpt = list[Math.floor(Math.random() * list.length)];
-  return computerOpt;
+    const result = await Player.getResult(player1.getOpt, computer.getOpt, player1.getName, computer.getName);
+    const resultCSS = document.getElementsByClassName('versus');
+    resultCSS.item(0).style.color = 'white';
+    resultCSS.item(0).style.backgroundColor = '#4C9654';
+    resultCSS.item(0).style.fontSize = '38px';
+    resultCSS.item(0).style.borderRadius = '10px';
+    resultCSS.item(0).style.width = '270px';
+    resultCSS.item(0).style.height = '160px';
+    resultCSS.item(0).style.display = 'flex';
+    resultCSS.item(0).style.justifyContent = 'center';
+    resultCSS.item(0).style.alignItems = 'center';
+    resultCSS.item(0).style.textAlign = 'center';
+    resultCSS.item(0).style.transform = 'rotate(-10deg)';
+    resultCSS.item(0).innerHTML = result;
+  }, 1000);
+  
 }
 
 startGame();
